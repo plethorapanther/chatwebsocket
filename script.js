bdf222d4431e8090eb55c8ae20f8f8751bc6a068
plethorapanther <97998646+plethorapanther@users.noreply.github.com>
2023-11-14 10:50:12 -0700

    Trying to get git working

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+let socket = new WebSocket("wss://javascript.info/article/websocket/chat/ws");
+
+// send message from the form
+document.forms.publish.onsubmit = function() {
+    let outgoingMessage = this.message.value;
+
+    socket.send(outgoingMessage);
+    return false;
+};
+
+// message received - show the message in div#messages
+socket.onmessage = function(event) {
+    let message = event.data;
+
+    let messageElem = document.createElement('div');
+    messageElem.textContent = message;
+    document.getElementById('messages').prepend(messageElem);
+};
